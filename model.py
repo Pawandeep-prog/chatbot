ab3f3d0707990fd8f5395e8e4c67f1b3293e4b2f
Programminghut <58453222+Pawandeep-prog@users.noreply.github.com>
2020-10-06 19:33:58 -0700

    Update model.py

--- model.py
+++ model.py
@@ -22,15 +22,13 @@ enc_states = [h, c]
 
 dec_embed = embed(dec_inp)
 dec_lstm = LSTM(400, return_sequences=True, return_state=True)
-dec_op, _, _ = enc_lstm(enc_embed)
+dec_op, _, _ = dec_lstm(dec_embed, initial_state=enc_states)
 
 dense = Dense(VOCAB_SIZE, activation='softmax')
 
 dense_op = dense(dec_op)
 
 model = Model([enc_inp, dec_inp], dense_op)
-
-
 
 
 
